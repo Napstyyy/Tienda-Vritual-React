c8997f3afa1e2945fea98e454bc4312414c90a75
Napstyyy <mate2003-4@outlook.com>
2023-01-28 08:24:07 -0500

    creo que ahora el front funciona

---
+++ cliente/src/pages/login/login.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import './login.css'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import encriptador from "../../encripter";
+
+const URI = 'http://localhost:3001/users/';
+
+const Login = () => {
+    const navigate = useNavigate();
+    const navigateRegister = () => {
+        navigate(`/register`);
+    }
+
+    const navigateLogin = () => {
+        navigate(`/login`);
+    }
+
+    const navigateShopAddtoCart = () => {
+        navigate(`/shop`);
+    }
+
+    const [entrada, SetEntrada] = useState('');
+    const [entradaP, SetEntradaP] = useState('');
+    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = async() => {
+        const res = await axios.get(URI)
+        setUsers(res.data)
+    }
+
+    const compare = () => {
+        users.map((user) => {
+            if ((user.username === entrada) && (user.password === entradaP)) {
+                navigate('/shop');
+            }
+        })
+    }
+
+    return (
+        <div className="login-form">
+            <h2>Login</h2>
+            <form>
+                <input 
+                    value={entrada}
+                    onChange={(e) => SetEntrada(e.target.value)}
+                    type="text" name="user" id="user" placeholder="user" />
+                <input 
+                    value={entradaP}
+                    onChange={(e) => SetEntradaP(encriptador.enmascarador(e.target.value))}
+                    type="password" name="pass" id="pass" placeholder="password" />
+                <input type="submit" className="btn-login" value="Login" onClick={( compare()? navigateShopAddtoCart : navigateLogin)}/>
+            </form>
+            <div href="register" className="btn-register" onClick={navigateRegister}>register</div>
+        </div>
+    )
+}
+
+export default Login;
